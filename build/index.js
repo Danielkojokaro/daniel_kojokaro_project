66d93cab1eafff825b17ad460f02b82b9a0e8748
Bar Shaya <barsh@avantisteam.com>
2022-06-19 17:36:24 +0300

    .

---
+++ build/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+document.getElementById("footer").innerHTML = "Daniel Kojokaro - 205817158 - danielkojokaro@gmail.com"
+
+function clickMeButton() {
+    alert("Hi! It's Ex 4 page")
+}
+
---
+++ build/static/js/main.eb55f1e2.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.getElementById("footer").innerHTML="Daniel Kojokaro - 205817158 - danielkojokaro@gmail.com"}();
+//# sourceMappingURL=main.eb55f1e2.js.map
