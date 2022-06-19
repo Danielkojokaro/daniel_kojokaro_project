9ebf13bc329e7bb4254c51b85aacc8d46b9f03ed
Bar Shaya <barsh@avantisteam.com>
2022-06-19 17:48:31 +0300

    fix photos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import './index.css';
-document.getElementById("footer").innerHTML = "Daniel Kojokaro - 205817158 - danielkojokaro@gmail.com"
-
-function clickMeButton() {
-    alert("Hi! It's Ex 4 page")
-}
-
+import "./index.css";
+document.getElementById("footer").innerHTML =
+  "Daniel Kojokaro - 205817158 - danielkojokaro@gmail.com";
